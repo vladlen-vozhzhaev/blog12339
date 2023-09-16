fd63ec8e4beb0995c0beff1ef8f10d4fc38602c3
Vlalden <vladlen@vozhzhaev.ru>
2023-09-16 17:05:37 +0300

    change blog

--- php/classes/Blog.php
+++ php/classes/Blog.php
@@ -1,1 +1,28 @@
 <?php
+    class Blog{
+        // Добавление статьи
+        public static function addArticle(){
+            global $mysqli;
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $author = $_POST['author'];
+            $mysqli->query("INSERT INTO `articles`(`title`, `content`, `author`) VALUES ('$title','$content','$author')");
+            header("Location: /");
+        }
+        // Удаление статьи
+        public static function deleteArticle(){
+
+        }
+        // Редактирование статьи
+        public static function editArticle(){
+
+        }
+        // Отдать все статьи в формате JSON
+        public static function getArticles(){
+
+        }
+        // Отдать одну статью по ID
+        public static function getArticle(){
+
+        }
+    }
--- router.php
+++ router.php
@@ -4,6 +4,7 @@
     $method = $_SERVER['REQUEST_METHOD'];
     $mysqli = new mysqli('127.0.0.1', 'root', '',  'blog12339');
     require_once('php/classes/User.php');
+    require_once('php/classes/Blog.php');
     $content =  "Страница не существует 404";
     if($path == "/login" && $method=='GET'){
         $content = file_get_contents('views/login.html');
@@ -21,6 +22,9 @@
         exit(User::getCurrentUser());
     }else if($path == '/uploadUserAvatar'){
         exit(User::uploadUserAvatar());
+    }else if($path == '/addArticle' && $method == 'GET'){
+        $content = file_get_contents('views/addArticle.html');
+    }else if($path == '/addArticle' && $method == 'POST'){
+        exit(Blog::addArticle());
     }
-
     require_once 'views/template.php';
