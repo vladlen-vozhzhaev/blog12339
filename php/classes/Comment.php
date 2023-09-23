0b43375e60b04bdbbc73acbec8f13a89213c5698
Vlalden <vladlen@vozhzhaev.ru>
2023-09-23 17:08:02 +0300

    upd

---
+++ php/classes/Comment.php
@@ -0,0 +1,15 @@
+<?php
+class Comment{
+    public function addComment(){
+
+    }
+    public function getComments(){
+
+    }
+    public function deleteComment(){
+
+    }
+    public function editComment(){
+
+    }
+}
